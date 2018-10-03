f63acb9ff1cb1e683f87577c284469fcad29e540
TechartWebDev <webdev@techart.ru>
2018-10-03 10:39:45 +0300

    Version 3.7.4

--- src/TAO/Text/Processor/Insertions.php
+++ src/TAO/Text/Processor/Insertions.php
@@ -14,7 +14,11 @@ class Insertions implements ProcessorInterface
 			$insertion = config("tao.insertions.{$name}");
 			if ($insertion) {
 				$args = [];
-				$namedArgs = ['insertion_source' => $m[0]];
+				$namedArgs = [
+					'insertion_source' => $m[0],
+					'insertion_code' => $m[1],
+					'insertion_args' => $m[2],
+				];
 				$unnamedArgs = [];
 				$callback = false;
 				if (is_array($insertion) && isset($insertion['action'])) {
@@ -25,7 +29,7 @@ class Insertions implements ProcessorInterface
 
 				if (is_string($callback) || \TAO\Callback::isValidCallback($callback)) {
 					if (is_array($insertion) && isset($insertion['params'])) {
-						$args = $namedArgs = $insertion['params'];
+						$args = $namedArgs = array_merge($namedArgs, $insertion['params']);
 					}
 					foreach (explode("\n", $arg) as $line) {
 						foreach (explode(';', $line) as $item) {
