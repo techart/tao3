aee22ae2997e00672a61d93d91b3cce54536e2a7
Git Techart <git@tchart.ru>
2024-05-15 11:28:57 +0000

    Version 11.0.4

--- resources/views/table/list/branch-title.blade.php
+++ resources/views/table/list/branch-title.blade.php
@@ -1,15 +1,18 @@
 <td class="admin-tree-title">
-  <div class="bullet bullet-level-{{ $level }}">&nbsp;</div>
-  @if ($row->checkIfSortable())
-    <div class="controls">
-      @if (!$row->isFirstBranch)
-        <a class="control" href="{{ url($controller->actionUrl('weight', ['id' => $row->getKey(), 'with' => $row->prevBranch->getKey() ])) }}"><i class="up">&nbsp;</i></a>
-      @endif
-      @if (!$row->isLastBranch)
-        <a class="control" href="{{ url($controller->actionUrl('weight', ['id' => $row->getKey(), 'with' => $row->nextBranch->getKey() ])) }}"><i class="down">&nbsp;</i></a>
-      @endif
-      &nbsp;
+  <div class="bullet bullet-level-{{ $level }}">
+    <div class="bullet-text">
+      {!! $row->titleForTreeAdmin($level) !!}
     </div>
-  @endif
-  {!! $row->titleForTreeAdmin($level) !!}
+    @if ($row->checkIfSortable())
+      <div class="controls">
+        @if (!$row->isFirstBranch)
+          <a class="control" href="{{ url($controller->actionUrl('weight', ['id' => $row->getKey(), 'with' => $row->prevBranch->getKey() ])) }}"><i class="up">&nbsp;</i></a>
+        @endif
+        @if (!$row->isLastBranch)
+          <a class="control" href="{{ url($controller->actionUrl('weight', ['id' => $row->getKey(), 'with' => $row->nextBranch->getKey() ])) }}"><i class="down">&nbsp;</i></a>
+        @endif
+        &nbsp;
+      </div>
+    @endif
+  </div>
 </td>
