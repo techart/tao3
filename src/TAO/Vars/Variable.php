d390ea4f6e9ae78041cf955e2348627b543acc6b
Git Techart <git@tchart.ru>
2024-10-21 07:40:20 +0000

    Version 11.0.6

--- src/TAO/Vars/Variable.php
+++ src/TAO/Vars/Variable.php
@@ -171,22 +171,22 @@ class Variable implements \ArrayAccess
 		return $this->values[$key] ?? null;
 	}
 
-	public function offsetExists($offset)
+	public function offsetExists(mixed $offset): bool
 	{
 		return isset($this->values[$offset]);
 	}
 
-	public function offsetGet($offset)
+	public function offsetGet(mixed $offset): mixed
 	{
 		return $this->values[$offset] ?? null;
 	}
 
-	public function offsetSet($offset, $value)
+	public function offsetSet(mixed $offset, mixed $value): void
 	{
 		$this->values[$offset] = $value;
 	}
 
-	public function offsetUnset($offset)
+	public function offsetUnset(mixed $offset): void
 	{
 		unset($this->values[$offset]);
 	}
