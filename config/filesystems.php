3395d0689e3b521c2d02c0b369b2c1a1887820ea
Git Techart <git@tchart.ru>
2024-12-06 07:51:04 +0000

    Version 11.0.7

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,6 +46,18 @@ return [
 		'local' => [
 			'driver' => 'local',
 			'root' => storage_path('app'),
+			'visibility' => 'public',
+			'directory_visibility' => 'public',
+			'permissions' => [
+				'file' => [
+					'public' => 0664,
+					'private' => 0600,
+				],
+				'dir' => [
+					'public' => 0775,
+					'private' => 0700,
+				],
+			],
 		],
 
 		'public' => [
@@ -53,6 +65,17 @@ return [
 			'root' => storage_path('app/public'),
 			'url' => env('APP_URL') . '/storage',
 			'visibility' => 'public',
+			'directory_visibility' => 'public',
+			'permissions' => [
+				'file' => [
+					'public' => 0664,
+					'private' => 0600,
+				],
+				'dir' => [
+					'public' => 0775,
+					'private' => 0700,
+				],
+			],
 		],
 
 		's3' => [
